4ac95f46e90b3e9f3419661c11199d56322f000f
Arsalaan-Alam <studyfreak1234@gmail.com>
2023-07-17 01:12:29 +0530

    added select functionality

--- flowtify-client/app/send/page.js
+++ flowtify-client/app/send/page.js
@@ -3,19 +3,14 @@ import React, { useEffect, useState } from "react";
 import * as fcl from "@onflow/fcl";
 import "@/flow/config";
 
-
-
-
 const LibraryPage = () => {
-  //const walletAddresses = ["0x0fb74b342aea7798", "0xf29693609c4d4494", "0xb5bd1bfcd1f36235", "0xfac4f77afa0cd121", "0xbb929b5de40a563c", "0xeead9ebbca30dc2c"];
-  //const walletAddresses = ["0x0fb74b342aea7798"];
   const [profiles, setProfiles] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const [searchQuery, setSearchQuery] = useState("");
-  const [filteredProfiles, setFilteredProfiles] = useState("");
+  const [filteredProfiles, setFilteredProfiles] = useState([]);
+  const [selectedProfiles, setSelectedProfiles] = useState([]);
 
   const getProfileData = async () => {
-    
     const profileData = await fcl.query({
       cadence: `
         import Library from 0xf41fd3cb80a5dce4
@@ -23,23 +18,21 @@ const LibraryPage = () => {
         pub fun main(): [{Address: Library.Profile}] {
           return Library.getAllProfiles()
         }
-      `,      
+      `,
     });
-    console.log(profileData)
     const transformedData = profileData.map((obj) => {
       const [address, value] = Object.entries(obj)[0];
       return { address, ...value };
     });
-    console.log(transformedData)
-    return transformedData
-  }
+    return transformedData;
+  };
 
-  useEffect(async() => {     
-    const profileData = await getProfileData()
-    setProfiles(profileData)  
-    setFilteredProfiles(profileData)
+  useEffect(async () => {
+    const profileData = await getProfileData();
+    setProfiles(profileData);
+    setFilteredProfiles(profileData);
     setIsLoading(false);
-  },[])
+  }, []);
 
   const handleSearch = (e) => {
     setSearchQuery(e.target.value);
@@ -48,13 +41,30 @@ const LibraryPage = () => {
         profile?.fullname.toLowerCase().includes(searchQuery.toLowerCase()) ||
         profile?.username.toLowerCase().includes(searchQuery.toLowerCase()) ||
         profile?.desc.toLowerCase().includes(searchQuery.toLowerCase())
-    );    
-    setFilteredProfiles(searchedProfiles)
+    );
+    setFilteredProfiles(searchedProfiles);
   };
 
-  
-  
-    
+  const toggleSelect = (index) => {
+    const updatedProfiles = [...filteredProfiles];
+    updatedProfiles[index].isSelected = !updatedProfiles[index].isSelected;
+    setFilteredProfiles(updatedProfiles);
+
+    const selectedProfile = updatedProfiles[index];
+    if (selectedProfile.isSelected) {
+      setSelectedProfiles((prevSelectedProfiles) => [
+        ...prevSelectedProfiles,
+        selectedProfile.address,
+      ]);
+    } else {
+      setSelectedProfiles((prevSelectedProfiles) =>
+        prevSelectedProfiles.filter(
+          (address) => address !== selectedProfile.address
+        )
+      );
+    }
+  };
+
   return (
     <div className="">
       <div className="flex items-center justify-center my-5">
@@ -65,40 +75,55 @@ const LibraryPage = () => {
           value={searchQuery}
           onChange={handleSearch}
         />
-        
       </div>
-     <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 mx-10 mb-10">
-{isLoading ? (
-       <div className="flex col-span-3 items-center justify-center mt-20">
-       Loading..
-     </div>
-      ) : (
-        filteredProfiles.map((profile, index) => (
-        <div
-          key={index}
-          className="bg-gray-100 shadow-md rounded-lg p-4 flex flex-col justify-between mt-5"
-        >
-          <div>
-            <img
-              src={profile?.avatar}
-              alt="profileimg"
-              className="w-40 h-40 rounded-full mx-auto"
-            />
-            <h2 className="text-xl font-bold mt-4 text-center">
-              {profile?.fullname}
-            </h2>
-            <p className="text-gray-600 text-center">{profile?.username}</p>
-           
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 mx-10 mb-10">
+        {isLoading ? (
+          <div className="flex col-span-3 items-center justify-center mt-20">
+            Loading..
           </div>
-          <div className="flex items-center justify-center">
-            
-            <a  href={`/profile/${profile?.address}`} className="mt-3 text-blue-500 font-semibold">View Profile</a>
-            
-          </div>
-        </div>
-      ))
-      )}
-    </div>
+        ) : (
+          filteredProfiles.map((profile, index) => (
+            <div
+              key={index}
+              className="bg-gray-100 shadow-md rounded-lg p-4 flex flex-col justify-between mt-5"
+            >
+              <div>
+                <img
+                  src={profile?.avatar}
+                  alt="profileimg"
+                  className="w-40 h-40 rounded-full mx-auto"
+                />
+                <h2 className="text-xl font-bold mt-4 text-center">
+                  {profile?.fullname}
+                </h2>
+                <p className="text-gray-600 text-center">
+                  {profile?.username}
+                </p>
+              </div>
+              <div className="flex flex-col items-center mt-3">
+                <a
+                  href={`https://flocial.vercel.app/profile/${profile?.address}`}
+                  className="text-blue-500 font-semibold mb-5"
+                >
+                  View Profile
+                </a>
+                <button
+  className={`bg-${profile.isSelected ? 'red' : 'blue'}-500 hover:bg-${profile.isSelected ? 'red' : 'blue'}-700 text-white font-bold py-2 px-4 rounded`}
+  onClick={() => toggleSelect(index)}
+>
+  {profile.isSelected ? "Remove" : "Select"}
+</button>
+              </div>
+            </div>
+          ))
+        )}
+      </div>
+      <div>
+        <h2>Selected Profiles:</h2>
+        {selectedProfiles.map((address, index) => (
+          <div key={index}>{address}</div>
+        ))}
+      </div>
     </div>
   );
 };

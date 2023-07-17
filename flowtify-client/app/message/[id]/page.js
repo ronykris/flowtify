720a48cc46ede420a43cd1b1bf3b01acf9fb68c5
Arsalaan-Alam <studyfreak1234@gmail.com>
2023-07-17 17:06:58 +0530

    some css changes

--- flowtify-client/app/message/[id]/page.js
+++ flowtify-client/app/message/[id]/page.js
@@ -14,9 +14,9 @@ export default function Message() {
   }
 
   return (
-    <div class="flex items-center justify-center p-12">
-      <div class="w-full max-w-xs">
-        <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+    <div class="flex items-center justify-center p-12 pt-36">
+      <div class="w-full max-w-2xl">
+        <form class="bg-gray-100 shadow-md rounded px-8 pt-6 pb-8 mb-4">
           <div class="mb-4">
             <label class="block text-gray-700 text-sm font-bold mb-2" for="receivers">
               Receivers
@@ -27,7 +27,7 @@ export default function Message() {
             <label class="block text-gray-700 text-sm font-bold mb-2" for="msg">
               Message
             </label>
-            <textarea class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="msg" type="text" placeholder="Enter your message" />
+            <textarea class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline h-32" id="msg" type="text" placeholder="Enter your message" />
           </div>
           <div class="flex items-center justify-between">
             <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
